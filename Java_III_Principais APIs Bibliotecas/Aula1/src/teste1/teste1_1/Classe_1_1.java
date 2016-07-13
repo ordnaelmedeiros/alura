3d08855b09ad486c30c1d2b396559c7c33c0d819
ordnaelmedeiros <lrmarkanjo@gmail.com>
2016-07-12 22:50:01 -0300

    Java III Aula 1

---
+++ Java_III_Principais APIs Bibliotecas/Aula1/src/teste1/teste1_1/Classe_1_1.java
@@ -0,0 +1,11 @@
+package teste1.teste1_1;
+
+import teste1.teste1_1.teste1_1_1.Classe_1_1_1;
+
+public class Classe_1_1 {
+
+	protected Classe_1_1() {
+		//Classe_1_1_1 classe = new Classe_1_1_1();
+	}
+	
+}
---
+++ Java_III_Principais APIs Bibliotecas/Aula1/src/teste1/teste1_2/Classe_1_2.java
@@ -0,0 +1,11 @@
+package teste1.teste1_2;
+
+import teste1.teste1_1.teste1_1_1.Classe_1_1_1;
+
+public class Classe_1_2 {
+
+	protected Classe_1_2() {
+		//Classe_1_1_1 classe = new Classe_1_1_1()
+	}
+	
+}
