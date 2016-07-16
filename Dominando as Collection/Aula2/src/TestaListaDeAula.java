108710fbddb4945ba624b7be49f584594e779439
ordnaelmedeiros <lrmarkanjo@gmail.com>
2016-07-15 22:37:08 -0300

    Collections Aula 2

---
+++ Dominando as Collection/Aula2/src/TestaListaDeAula.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestaListaDeAula {
+
+	public static void main(String[] args) {
+
+		Aula a1 = new Aula("Revistando as ArrayLists", 21);
+		Aula a2 = new Aula("Listas de objetos", 20);
+		Aula a3 = new Aula("Relacionamento de listas e objetos", 15);
+
+		List<Aula> aulas = Arrays.asList(a1, a2, a3);
+		
+		System.out.println(aulas);
+		
+		aulas.sort(Comparator.comparing(Aula::getTempo));
+		
+		System.out.println(aulas);
+		
+	}
+
+}
