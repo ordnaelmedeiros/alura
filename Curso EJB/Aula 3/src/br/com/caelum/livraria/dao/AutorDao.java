6df4e90b7b5c1f224c0b9269e90875c770bf27ea
ordnaelmedeiros <lrmarkanjo@gmail.com>
2016-11-29 23:42:51 -0200

    EJB Aula 7

--- Curso EJB/Aula 3/src/br/com/caelum/livraria/dao/AutorDao.java
+++ Curso EJB/Aula 3/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,12 +4,20 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import br.com.caelum.livraria.interceptador.LogInterceptador;
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)  // Opcional
+//@Interceptors({ LogInterceptador.class })
 public class AutorDao {
 
 	@PersistenceContext
@@ -24,6 +32,7 @@ public class AutorDao {
 		System.out.println("[INFO] AutorDao foi iniciado.");
 	}
 	
+	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public void salva(Autor autor) {
 		
 		System.out.println("[INFO] Salvando o Autor " + autor.getNome());
---
+++ Curso EJB/Aula 3/src/br/com/caelum/livraria/dao/AutorService.java
@@ -0,0 +1,28 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorService {
+
+    @Inject
+    AutorDao dao;
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void adiciona(Autor autor) {
+        this.dao.salva(autor);
+        throw new RuntimeException("[ERRO] Erro lançado para testar o rollback da transação.");
+    }
+
+    public List<Autor> todosAutores() {
+        return this.dao.todosAutores();
+    }
+    
+}
