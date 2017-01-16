93c7661dedc7b727e86c04806a6705d9b0312136
ordnaelmedeiros <lrmarkanjo@gmail.com>
2017-01-16 21:23:59 -0200

    threads

---
+++ Threads/calculador/src/br/com/alura/threads/AcaoBotao.java
@@ -0,0 +1,47 @@
+
+package br.com.alura.threads;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigInteger;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class AcaoBotao implements ActionListener {
+
+	private JTextField primeiro;
+	private JTextField segundo;
+	private JLabel resultado;
+
+	public AcaoBotao(JTextField primeiro, JTextField segundo, JLabel resultado) {
+		this.primeiro = primeiro;
+		this.segundo = segundo;
+		this.resultado = resultado;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		/*
+		long valor1 = Long.parseLong(primeiro.getText());
+		long valor2 = Long.parseLong(segundo.getText());
+		BigInteger calculo = new BigInteger("0");
+		
+		for (int i = 0; i < valor1; i++) {
+			for (int j = 0; j < valor2; j++) {
+				calculo = calculo.add(new BigInteger("1"));
+			}
+		}
+
+		resultado.setText(calculo.toString());
+		*/
+		
+		TarefaMultiplicacao tarefa = new TarefaMultiplicacao(primeiro, segundo, resultado);
+	    Thread threadMultiplicador = new Thread(tarefa);
+
+	    //thread começa a trabalhar
+	    threadMultiplicador.start();
+	    
+	}
+
+}
