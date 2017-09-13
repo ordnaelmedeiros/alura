9dea47e030264a9ed70e302aa5d48a2154cb7ffe
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2017-09-12 21:43:45 -0300

    Thread 1 aula 5

---
+++ Threads/Threads_1_aula05_banheiro/src/br/com/alura/banheiro/Banheiro.java
@@ -0,0 +1,109 @@
+package br.com.alura.banheiro;
+
+public class Banheiro {
+
+	private boolean estaSujo = true;
+	
+	public void fazNumero1() {
+		
+		String nome = Thread.currentThread().getName();
+		System.out.println(nome + " bateu na porta");
+
+		synchronized (this) {
+			
+			System.out.println(nome + " entrando no banheiro");
+			
+			this.verificaBaneiroSujo(nome);
+			
+			System.out.println(nome + " fazendo coisa rapida");
+	
+			try {
+				Thread.sleep(8000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			this.estaSujo = true;
+	
+			System.out.println(nome + " dando descarga");
+			System.out.println(nome + " lavando a mao");
+			System.out.println(nome + " saindo do banheiro");
+
+		}
+		
+	}
+
+	public void fazNumero2() {
+
+		String nome = Thread.currentThread().getName();
+		System.out.println(nome + " bateu na porta");
+		
+		synchronized (this) {
+			
+			System.out.println(nome + " entrando no banheiro");
+			
+			this.verificaBaneiroSujo(nome);
+			
+			System.out.println(nome + " fazendo coisa demorada");
+	
+			try {
+				Thread.sleep(15000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	
+			this.estaSujo = true;
+			
+			System.out.println(nome + " dando descarga");
+			System.out.println(nome + " lavando a mao");
+			System.out.println(nome + " saindo do banheiro");
+		
+		}
+		
+	}
+	
+	public void limpa() {
+
+	    String nome = Thread.currentThread().getName();
+
+	    System.out.println(nome + " batendo na porta");
+
+	    synchronized (this) {
+
+	        System.out.println(nome + " entrando no banheiro");
+
+	        if (!this.estaSujo) {
+	            System.out.println(nome + ", não está sujo, vou sair");
+	            return;
+	        }
+
+	        System.out.println(nome + " limpando o banheiro");
+	        this.estaSujo = false;
+
+	        try {
+	            Thread.sleep(13000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+
+	        this.notifyAll();
+
+	        System.out.println(nome + " saindo do banheiro");
+	    }
+	}
+	
+	private void verificaBaneiroSujo(String nome) {
+
+		while (this.estaSujo) {
+			
+		    System.out.println(nome + ", eca, banheiro está sujo");
+		    try {
+		        this.wait();
+		    } catch (InterruptedException e) {
+		        e.printStackTrace();
+		    }
+		    
+		}
+	}
+
+}
