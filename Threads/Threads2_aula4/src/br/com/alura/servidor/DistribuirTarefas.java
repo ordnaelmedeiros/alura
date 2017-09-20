6ca6ca37db514732af7eab22841c333950990831
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2017-09-19 22:25:43 -0300

    Threads 2 aula 4

---
+++ Threads/Threads2_aula4/src/br/com/alura/servidor/DistribuirTarefas.java
@@ -0,0 +1,62 @@
+package br.com.alura.servidor;
+
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class DistribuirTarefas implements Runnable {
+
+	private Socket socket;
+    private ServidorTarefas servidor; //novo atributo
+
+    // recebendo servidor
+    public DistribuirTarefas(Socket socket, ServidorTarefas servidor) {
+        this.socket = socket;
+        this.servidor = servidor; //novo
+    }
+    
+    @Override
+    public void run() {
+
+        try {
+            System.out.println("Distribuindo as tarefas para o cliente " + socket);
+
+            Scanner entradaCliente = new Scanner(socket.getInputStream());
+            PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+
+            while (entradaCliente.hasNextLine()) {
+
+                String comando = entradaCliente.nextLine();
+                System.out.println("Comando recebido " + comando);
+
+                switch(comando) {
+                case "c1" : {
+                    saidaCliente.println("Confirmação do comando: " + comando);
+                    break;
+                }
+                case "c2" : {
+                    saidaCliente.println("Confirmação do comando: " + comando);
+                    break;
+                }
+                case "fim" : {
+                    saidaCliente.println("Desligando o servidor");
+                    servidor.parar();
+                    break;
+                }
+                default : {
+                    saidaCliente.println("Comando não encontrado");
+                }
+            }
+
+                System.out.println(comando);
+            }
+
+            saidaCliente.close();
+            entradaCliente.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
