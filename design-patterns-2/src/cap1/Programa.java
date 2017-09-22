78c294954b72d951beb408260612a51ef7132080
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2017-09-22 00:31:25 -0300

    dp 2 aula 1

--- design-patterns-2/src/cap1/Programa.java
+++ design-patterns-2/src/cap1/Programa.java
@@ -1,17 +1,17 @@
 package cap1;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Programa {
 
     public static void main(String[] args) throws SQLException {
-        Connection conexao = 
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "usuario", "senha");
+    	
+        Connection conexao = new ConnectionFactory().getConnection();
 
         PreparedStatement ps = conexao.prepareStatement("select * from ...");
         // codigo continua aqui
     }
+    
 }
