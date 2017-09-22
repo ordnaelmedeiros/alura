2fdc46562b2fb8637230931a0f79a7517eed62a6
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2017-09-22 00:09:15 -0300

    Thread 2 aula 7

---
+++ Threads/Threads2_aula7/src/br/com/alura/servidor/DistribuirTarefas.java
@@ -0,0 +1,88 @@
+package br.com.alura.servidor;
+
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class DistribuirTarefas implements Runnable {
+
+	private Socket socket;
+	private ServidorTarefas servidor; // novo atributo
+	private ExecutorService threadPool;
+	private BlockingQueue<String> filaComandos;
+
+	// recebendo servidor
+	public DistribuirTarefas(ExecutorService threadPool, BlockingQueue<String> filaComandos, Socket socket, ServidorTarefas servidor) {
+		this.threadPool = threadPool;
+		this.filaComandos = filaComandos;
+		this.socket = socket;
+		this.servidor = servidor; // novo
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			System.out.println("Distribuindo as tarefas para o cliente " + socket);
+
+			Scanner entradaCliente = new Scanner(socket.getInputStream());
+			PrintStream saidaCliente = new PrintStream(socket.getOutputStream());
+
+			while (entradaCliente.hasNextLine()) {
+
+				String comando = entradaCliente.nextLine();
+				System.out.println("Comando recebido " + comando);
+
+				switch (comando) {
+				case "c1": {
+					saidaCliente.println("Confirmação do comando: " + comando);
+					ComandoC1 c1 = new ComandoC1(saidaCliente); // criando comando
+					this.threadPool.execute(c1);
+					break;
+				}
+				case "c2": {
+					saidaCliente.println("Confirmação do comando c2");
+
+					// criando os dois comandos
+					ComandoC2ChamaWs c2WS = new ComandoC2ChamaWs(saidaCliente);
+					ComandoC2AcessaBanco c2Banco = new ComandoC2AcessaBanco(saidaCliente);
+
+					// passando os comandos para o pool, resultado é um Future
+					Future<String> futureWS = this.threadPool.submit(c2WS);
+					Future<String> futureBanco = this.threadPool.submit(c2Banco);
+
+					// novo, passando a tarefa para juntar os resultados para o pool
+					this.threadPool.submit(new JuntaResultadosFutureWSFutureBanco(futureWS, futureBanco, saidaCliente));
+
+					break;
+				}
+				case "c3" : {
+				    this.filaComandos.put(comando); //lembrando, bloqueia se tiver cheia
+				    saidaCliente.println("Comando c3 adicionado na fila");
+				        break;
+				}
+				case "fim": {
+					saidaCliente.println("Desligando o servidor");
+					servidor.parar();
+					break;
+				}
+				default: {
+					saidaCliente.println("Comando não encontrado");
+				}
+				}
+
+				System.out.println(comando);
+			}
+
+			saidaCliente.close();
+			entradaCliente.close();
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
