08befe7b43cbd223ee388dcdbaaaa920b771c418
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2017-10-24 20:34:17 -0200

    refatoracao cap 5

--- alura-refatoracao/src/br/alura/refatoracao/cap5/Fatura.java
+++ alura-refatoracao/src/br/alura/refatoracao/cap5/Fatura.java
@@ -24,9 +24,14 @@ public class Fatura {
 		this.cliente = cliente;
 	}
 
-	public double converteValor(boolean dolar) {
-		double taxa = 1;
-		if (dolar) taxa = 2.7;
+	private double converte(double taxa) {
 		return valorMensal * taxa;
 	}
+	public double emReal() {
+		return converte(1.0);
+	}
+	public double emDolar() {
+		return converte(3.8);
+	}
+	
 }
