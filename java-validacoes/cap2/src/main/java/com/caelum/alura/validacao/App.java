d13590c4263d174db5b909b47dad0fa65442c86e
Leandro Medeiros <ordnaelmedeiros@gmail.com>
2017-12-14 21:37:11 -0200

    validacao cap 2

---
+++ java-validacoes/cap2/src/main/java/com/caelum/alura/validacao/App.java
@@ -0,0 +1,27 @@
+package com.caelum.alura.validacao;
+
+import br.com.caelum.stella.format.CPFFormatter;
+import br.com.caelum.stella.format.Formatter;
+import br.com.caelum.stella.validation.CPFValidator;
+import br.com.caelum.stella.validation.Validator;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		validar("CPF", new CPFValidator(), new CPFFormatter(), "00951953940");
+		
+	}
+	
+	private static void validar(String nome, Validator<String> validador, Formatter formater, String valor) {
+		
+		try {
+			validador.assertValid(valor);
+			System.out.println(nome+" VALIDO : " + formater.format(valor));
+		} catch (Exception e) {
+			System.out.println(nome+" INVALIDO : " + e);
+		}
+		
+	}
+	
+}
---
+++ java-validacoes/cap2/src/main/java/com/caelum/alura/validacao/NumeroPorExtenso.java
@@ -0,0 +1,17 @@
+package com.caelum.alura.validacao;
+
+import java.math.BigDecimal;
+
+import br.com.caelum.stella.inwords.FormatoDeReal;
+import br.com.caelum.stella.inwords.NumericToWordsConverter;
+
+public class NumeroPorExtenso {
+	
+	public static void main(String[] args) {
+		NumericToWordsConverter conversor = new NumericToWordsConverter(new FormatoDeReal());
+		BigDecimal valor = new BigDecimal("902.59");
+		String valorPorExtenso = conversor.toWords(valor.doubleValue());
+		System.out.println(valorPorExtenso);
+	}
+
+}
