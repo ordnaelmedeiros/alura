dd283bd82e835d1fba9430b6cce99a6f7952e761
Leandro Medeiros <ordnaelmedeiros@gmail.com>
2017-12-28 22:19:45 -0200

    listavip aula 2

--- listavip/src/main/java/br/com/alura/listavip/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/Configuracao.java
@@ -1,24 +1,29 @@
 package br.com.alura.listavip;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
-@Controller
 public class Configuracao {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String ola() {
-		return "<h1>Olá. Está tudo funcionando</h1>";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
 	}
+	
+	@Bean
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/listavip");
+		dataSource.setUsername("postgres");
+		dataSource.setPassword("123456");
+		return dataSource;
+	}
+	
+	
 
 }
