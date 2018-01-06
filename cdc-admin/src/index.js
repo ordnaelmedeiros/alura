d85fd4403b83e75115199bce219a452dbac8721d
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2018-01-06 00:26:15 -0200

    react aula 6

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import AutorBox from './Autor';
+//import LivroAdmin from './Livro';
+import Home from './Home';
+import './index.css';
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render((
+        <Router>
+            <App>
+                    <Switch>            
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/autor" component={AutorBox}/>
+                        <Route path="/livro" />                
+                    </Switch>            
+            </App>
+        </Router>
+
+), document.getElementById('root'));
