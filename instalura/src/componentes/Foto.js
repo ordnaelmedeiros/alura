62d36f749a717c119ec72c61df8b7c053d25517a
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2018-01-06 22:47:57 -0200

    react 2 aula 3

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -22,12 +22,12 @@ class FotoInfo extends Component {
               <div className="foto-info-likes">
                 {
                   this.props.foto.likers.map(liker => {
-                    return (<a href="#">{liker.login},</a>)
+                    return (<a key={liker.login} href="#">{liker.login},</a>)
                   })
                 }
-
+                 
                  curtiram
-
+             
               </div>
 
               <p className="foto-info-legenda">
@@ -39,7 +39,7 @@ class FotoInfo extends Component {
                 {
                   this.props.foto.comentarios.map(comentario => {
                     return (
-                      <li className="comentario">
+                      <li className="comentario" key={comentario.id}>
                         <a className="foto-info-autor">{comentario.login} </a>
                         {comentario.texto}
                       </li>
