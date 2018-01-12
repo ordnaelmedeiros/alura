66ece56ab95f2d0fc8bfac1d67228d85b9818eb1
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2018-01-11 23:04:23 -0200

    angular aula 4

--- alurapic/client/app/app.component.ts
+++ alurapic/client/app/app.component.ts
@@ -1,23 +1,9 @@
 import {Component} from '@angular/core';
-import {Http} from '@angular/http';
 
 @Component({
     selector: 'app',
     templateUrl: './app/app.component.html'
 })
 export class AppComponent {
-
-    fotos:Object[] = [];
-
-    constructor(http:Http) {
-
-        http.get('v1/fotos')
-        .map(res => res.json())
-        .subscribe(
-            fotos => this.fotos = fotos,
-            erro => console.log(erro)
-        );
-
-    }
 
 }
--- alurapic/client/app/app.module.ts
+++ alurapic/client/app/app.module.ts
@@ -1,14 +1,31 @@
 import 'rxjs/add/operator/map';
 
+import { routing } from './app.routes';
+
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+
 import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
+
+import { AppComponent }   from './app.component';
+import { ListagemComponent } from './listagem/listagem.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, FotoModule ],
-  declarations: [ AppComponent ],
+  imports:[
+    routing,
+    BrowserModule,
+    HttpModule,
+    FotoModule,
+    PainelModule
+  ],
+  declarations: [
+    AppComponent,
+    ListagemComponent,
+    CadastroComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule {
