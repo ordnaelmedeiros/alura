10f2a4d4ea12e2ffc833757e56809addd708851d
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2018-08-02 06:47:49 -0300

    machine learning aula 3

--- machine/classificacao/dados.py
+++ machine/classificacao/dados.py
@@ -8,11 +8,27 @@ def carregar_acessos():
     arquivo = open('acesso.csv', 'rb')
     leitor = csv.reader(arquivo)
 
-    leitor.next();
+    leitor.next()
 
     for home,como_funciona,contato,comprou in leitor :
 
         X.append([int(home),int(como_funciona),int(contato)])
+        Y.append(int(comprou))
+
+    return X, Y
+
+def carregar_buscas():
+
+    X = []
+    Y = []
+
+    arquivo = open('buscas.csv', 'rb')
+    leitor = csv.reader(arquivo)
+
+    leitor.next()
+
+    for home,busca,logado,comprou in leitor :
+        X.append([int(home),busca,int(logado)])
         Y.append(int(comprou))
 
     return X, Y
