0b8586a6260c339f46ee48a396f73613ee475a9d
ordnaelmedeiros <ordnaelmedeiros@gmail.com>
2018-08-03 06:33:04 -0300

    machine learning aula 4

--- machine/classificacao/classifica_buscas.py
+++ machine/classificacao/classifica_buscas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from collections import Counter
+
 df = pd.read_csv('buscas.csv')
 X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
@@ -20,19 +22,24 @@ treino_marcacoes = Y[:tamanho_de_treino]
 teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
+#eficacia do algoritmo que chuta tudo um unico valor
+acerto_base = max(Counter(teste_marcacoes).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
 
 resultado = modelo.predict(teste_dados)
 
-diferencas = resultado - teste_marcacoes
+acertos = (resultado==teste_marcacoes)
 
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
+##acertos = [d for d in diferencas if d == 0]
+total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
 
 taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-print(taxa_de_acerto)
-print(total_de_elementos)
+print("Taxa de acerto base : %f" % taxa_de_acerto_base)
+print("Taxa de acerto do algoritmo : %f" % taxa_de_acerto)
+print("Total de registros: %d" % total_de_elementos)
